dbd8b0afc863dd115d12d38836ed4abfd70ce606
Pavol Hejny <me@pavolhejny.com>
2022-05-11 09:25:15 +0200

    Working sample module

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,63 @@
 import {
-  declareModule,
-  ExtraJsxPlace,
-  makeExtrajsxModule,
-  randomEmoji,
-  React,
-  styled,
-  centerArts,
-  TextArt,
-} from "@collboard/modules-sdk";
-import helloWorldButton from "../assets/hello-world-button.png";
-import helloWorldIcon from "../assets/hello-world-icon.png";
-import {
-  name,
-  contributors,
-  description,
-  license,
-  repository,
-  version,
-} from "../package.json";
-import { Vector } from "xyzt";
+    centerArts,
+    declareModule,
+    patternToRegExp,
+    string_mime_type_with_wildcard,
+    TextArt,
+} from '@collboard/modules-sdk';
+import { Vector } from 'xyzt';
+import textFileIcon from '../assets/text-file-icon.svg';
+import { contributors, description, license, name, repository, version } from '../package.json';
 
-const mimeTypes: string_mime_type_with_wildcard[] = ["text/plain"];
+const mimeTypes: string_mime_type_with_wildcard[] = ['text/plain'];
 
-internalModules.declareModule({
-  manifest: {
-    name,
-    version,
-    description,
-    contributors,
-    license,
-    repository,
-    title: { en: "Sample button" },
-    categories: ["Productivity", "Buttons", "Template"],
-    icon: helloWorldIcon,
-    flags: {
-      isTemplate: true,
+declareModule({
+    manifest: {
+        name,
+        version,
+        description,
+        contributors,
+        license,
+        repository,
+        icon: textFileIcon,
+        flags: {
+            isTemplate: true,
+        },
+        supports: {
+            fileImport: mimeTypes,
+        },
+        // Note: Support modules do not need title or some nice public profile because they are typically installed automatically
     },
-    supports: {
-      fileImport:
-        mimeTypes /* <- TODO: [⭕] In future no need for fileImport: mimeTypes */,
+    async setup(systems) {
+        const { importSystem, appState } = await systems.request('importSystem', 'appState');
+
+        return importSystem.registerFileSupport({
+            priority: 0,
+
+            async processFile({ file, boardPosition, willCommitArts, next }) {
+                if (!mimeTypes.some((mimeType) => patternToRegExp(mimeType).test(file.type))) {
+                    return next();
+                }
+
+                willCommitArts();
+
+                const textArt = new TextArt(
+                    await file.text(),
+                    '#009edd',
+                    21 / appState.transform.scale.x,
+                    false,
+                    false,
+                    false,
+                    'none',
+                    Vector.zero(),
+                );
+
+                centerArts({ arts: [textArt], boardPosition });
+
+                return textArt;
+            },
+        });
     },
-  },
-  async setup(systems) {
-    const { importSystem, appState } = await systems.request(
-      "importSystem",
-      "appState"
-    );
-
-    return importSystem.registerFileSupport({
-      priority: 0,
-
-      async processFile({ file, boardPosition, willCommitArts, next }) {
-        if (
-          !mimeTypes.some((mimeType) =>
-            patternToRegExp(mimeType).test(file.type)
-          )
-        ) {
-          return next();
-        }
-
-        willCommitArts();
-
-        const textArt = new TextArt(
-          await file.text(),
-          "black",
-          21 / appState.transform.scale.x,
-          false,
-          false,
-          false,
-          "none",
-          Vector.zero()
-        );
-
-        centerArts({ arts: [textArt], boardPosition });
-
-        return textArt;
-      },
-    });
-  },
 });
 
 /**
